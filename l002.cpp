abe13351ec2e7d67dc078769097807e9605cd5b8
arko <arkajyotisaha891@gmail.com>
2022-07-29 11:37:39 +0530

    changes

---
+++ l002.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void file_i_o(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main(){
+    file_i_o();
+
+}
